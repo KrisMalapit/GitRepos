323f75cef7625731f9539d0657164edea2b7f4f3
kcmalapit <kcmalapit@WSD2012>
2021-03-18 06:41:35 +0800

    New SAF

---
+++ Git1/Git1/Utilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git1
+{
+    class Utilities
+    {
+        public int AddNew() {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+
+        }
+    }
+}
